b0ed437d31e526742f7b1e7feef2ffd9b812f5bb
Omar Ahmed <67840386+omarafekry@users.noreply.github.com>
2023-05-22 22:48:45 +0200

    added selection

--- DBApp.java
+++ DBApp.java
@@ -1,12 +1,4 @@
-package main;
-
 import java.util.*;
-import java.io.*;
-import java.util.Hashtable;
-
-import com.opencsv.CSVWriter;
-
-import exceptions.DBAppException;
 
 
 public class DBApp {
@@ -35,7 +27,101 @@ public class DBApp {
 							Hashtable<String,Object> htblColNameValue ) throws DBAppException {}
 	
 	public void deleteFromTable(String strTableName, Hashtable<String,Object> htblColNameValue) throws DBAppException{}
+    
+	@SuppressWarnings("all")
+	public Iterator selectFromTable(SQLTerm[] arrSQLTerms, String[] strarrOperators) throws DBAppException{
+		validateSelection(arrSQLTerms, strarrOperators);
+		LinkedList<Hashtable<String, Comparable<Object>>> result = getSingleRelation(arrSQLTerms[0]);
 
-	//public Iterator selectFromTable(SQLTerm[] arrSQLTerms, String[] strarrOperators) throws DBAppException{}
+		for (int i = 0; i < strarrOperators.length; i++) {
+			String operator = strarrOperators[i];
+			LinkedList<Hashtable<String, Comparable<Object>>> nextRelation = getSingleRelation(arrSQLTerms[i+1]);
+			if (operator.equals("AND")){
+				LinkedList<Hashtable<String, Comparable<Object>>> tempResult = new LinkedList<>();
+				for (Hashtable<String, Comparable<Object>> row	: result)
+					if (nextRelation.contains(row))
+						tempResult.addLast(row);
+				result = tempResult;
+			}
+			else{
+				for (Hashtable<String, Comparable<Object>> row	: nextRelation) 
+					if (!result.contains(row)) //i couldve changed the type to hashset but im lazy
+						result.addLast(row);
+			}
+		}
+
+		return result.iterator();
+	}
+
+	private LinkedList<Hashtable<String, Comparable<Object>>> getSingleRelation(SQLTerm term) throws DBAppException{
+		LinkedList<Hashtable<String, Comparable<Object>>> result = new LinkedList<>();
+
+		//check for index here
+
+		Table table = new Table(term._strTableName);
+		LinkedList<Hashtable<String, Comparable<Object>>> page;
+
+		while((page = table.nextPage()) != null){
+			switch(term._strOperator){
+				case ">":
+					for (Hashtable<String, Comparable<Object>> row : page) {
+						if (row.get(term._strColumnName).compareTo(term._objValue) > 0)
+							result.addLast(row);
+					}
+					break;
+				case ">=":
+					for (Hashtable<String, Comparable<Object>> row : page) {
+						if (row.get(term._strColumnName).compareTo(term._objValue) >= 0)
+							result.addLast(row);
+					}
+					break;
+				case "=":
+					for (Hashtable<String, Comparable<Object>> row : page) {
+						if (row.get(term._strColumnName).compareTo(term._objValue) == 0)
+							result.addLast(row);
+					}
+					break;
+				case "<=":
+					for (Hashtable<String, Comparable<Object>> row : page) {
+						if (row.get(term._strColumnName).compareTo(term._objValue) <= 0)
+							result.addLast(row);
+					}
+					break;
+				case "<":
+					for (Hashtable<String, Comparable<Object>> row : page) {
+						if (row.get(term._strColumnName).compareTo(term._objValue) < 0)
+							result.addLast(row);
+					}
+					break;
+			}
+			
+		}
+		return result;
+	}
+
+	private void validateSelection(SQLTerm[] arrSQLTerms, String[] strarrOperators) throws DBAppException{
+		for (SQLTerm term : arrSQLTerms) {
+			Column[] columns  = Table.getColumns(term._strTableName);
+			Column column = null;
+			boolean exists = false;
+			for (Column col : columns) 
+				if (col.name.equals(term._strColumnName)){
+					exists = true;
+					column = col;
+				}
+			if (!exists)
+				throw new DBAppException("Couldn't find a column with name " + term._strColumnName + " in table " + term._strTableName);
+			String objType = term._objValue.getClass().toString().split(" ")[1];
+			if (!objType.equals(column.type))
+				throw new DBAppException("Incorrect data type for column " + term._strColumnName + ". Expected " + column.type + " but found " + objType);
+			String op = term._strOperator;
+			if (!op.equals(">") && !op.equals(">=") && !op.equals("=") && !op.equals("<=") && !op.equals("<"))
+				throw new DBAppException("Incorrect operator " + op);			
+		}
+		for (String op : strarrOperators) {
+			if (!op.equals("AND") &&!op.equals("OR"))
+				throw new DBAppException("Incorrect operator " + op);
+		}
+	}
 	
 }
--- Table.java
+++ Table.java
@@ -2,34 +2,98 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
 import java.util.LinkedList;
 
 public class Table{
-    final static String delimitter = ",";
-    public static Column[] getColumns(String tableName) throws IOException{
+    private final static String delimitter = ",";
+    String name;
+    private int nextPage = 0;
+
+    public Table(String name){
+        this.name = name;
+    }
+    @SuppressWarnings("all")
+    public LinkedList<Hashtable<String, Comparable<Object>>> nextPage() throws DBAppException{
         BufferedReader br = null;
-        br = new BufferedReader(new FileReader(new File("metadata.csv")));
-        String line;
-        LinkedList<Column> columns = new LinkedList<>();
-        while((line = br.readLine()) == null){
-            String tName = line.split(delimitter)[0];
-            if (tName.equals(tableName)){
-                Column col = new Column();
-                col.name = line.split(delimitter)[1];
-                col.type = line.split(delimitter)[2];
-                col.clusteringKey = Boolean.parseBoolean(line.split(delimitter)[3]);
-                col.indexName = line.split(delimitter)[4];
-                col.indexType = line.split(delimitter)[5];
-                col.min = line.split(delimitter)[6];
-                col.max = line.split(delimitter)[7];
-                col.foreignKey = Boolean.parseBoolean(line.split(delimitter)[8]);
-                col.foreignTableName = line.split(delimitter)[9];
-                col.foreignColumnName = line.split(delimitter)[10];
-                col.computed = Boolean.parseBoolean(line.split(delimitter)[11]);
-                columns.addLast(col);
+        LinkedList<Hashtable<String, Comparable<Object>>> rows = new LinkedList<>();
+        Column[] columns = getColumns(name);
+        try {
+            br = new BufferedReader(new FileReader(new File("Tables/" + name + "/" + nextPage + ".csv")));
+            String line;
+            while((line = br.readLine()) != null){
+                String[] values = line.split(delimitter);
+                Hashtable<String, Comparable<Object>> row = new Hashtable<>();
+                for (int i = 0; i < columns.length; i++) {
+                    if (columns[i].type.equals("java.lang.Integer"))
+                        row.put(columns[i].name, (Comparable<Object>)(Object)((Integer)Integer.parseInt(values[i])));
+                    else if(columns[i].type.equals("java.lang.String"))
+                        row.put(columns[i].name, (Comparable<Object>)(Object)values[i]);
+                    else if(columns[i].type.equals("java.lang.Double"))
+                        row.put(columns[i].name, (Comparable<Object>)(Object)Double.parseDouble(values[i]));
+                    else if(columns[i].type.equals("java.util.Date")){
+                        SimpleDateFormat formatter = new SimpleDateFormat("DD.MM.YYYY");
+                        Date date = null;
+                        try {
+                            date = formatter.parse(values[i]);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        row.put(columns[i].name, (Comparable<Object>)(Object)date);
+                    }
+                }
+                rows.addLast(row);
             }
+            br.close();
+        } catch (IOException e) {
+            return null;
         }
-        br.close();
-        return (Column[])columns.toArray();
+        nextPage++;
+        return rows;
+    }
+    public void resetPages(){
+        nextPage = 0;
+    }
+
+    @SuppressWarnings("all")
+    public static Column[] getColumns(String tableName) throws DBAppException{
+        BufferedReader br = null;
+        LinkedList<Column> columns = new LinkedList<>();
+        try {
+            br = new BufferedReader(new FileReader(new File("metadata.csv")));
+        
+            String line;
+            columns = new LinkedList<>();
+            while((line = br.readLine()) != null){
+                String tName = line.split(delimitter)[0];
+                if (tName.equals(tableName)){
+                    Column col = new Column();
+                    col.name = line.split(delimitter)[1];
+                    col.type = line.split(delimitter)[2];
+                    col.clusteringKey = Boolean.parseBoolean(line.split(delimitter)[3]);
+                    col.indexName = line.split(delimitter)[4];
+                    col.indexType = line.split(delimitter)[5];
+                    col.min = line.split(delimitter)[6];
+                    col.max = line.split(delimitter)[7];
+                    col.foreignKey = Boolean.parseBoolean(line.split(delimitter)[8]);
+                    col.foreignTableName = line.split(delimitter)[9];
+                    col.foreignColumnName = line.split(delimitter)[10];
+                    col.computed = Boolean.parseBoolean(line.split(delimitter)[11]);
+                    columns.addLast(col);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new DBAppException("Couldn't find metadata.csv");
+        }
+
+        if (columns.isEmpty())
+            throw new DBAppException("Couldn't find table with name " + tableName);
+        Column[] result = new Column[columns.size()];
+        columns.toArray(result);
+        return result;
     }
 }
